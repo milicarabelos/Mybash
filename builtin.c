f5813fb3ee49904f1a2856de13bb076a50bb6eca
juan-dev <juan-dev@pop-os.localdomain>
2022-09-03 15:27:52 -0300

    function builtin_alone

--- builtin.c
+++ builtin.c
@@ -1,8 +1,8 @@
 #include <stdbool.h>
-
+#include <assert.h>
 #include "command.h"
 
-bool builtin_is_internal(scommand cmd);
+bool builtin_is_internal(scommand cmd){
 //Nacho
 /*
  * Indica si el comando alojado en `cmd` es un comando interno
@@ -10,25 +10,15 @@ bool builtin_is_internal(scommand cmd);
  * REQUIRES: cmd != NULL
  *
  */
+}
 
+bool builtin_alone(pipeline p){
+    assert(p != NULL);
+    return pipeline_length(p) == 1 && builtin_is_internal(pipeline_front(p));
+}
 
-bool builtin_alone(pipeline p);
-//Juan
-/*
- * Indica si el pipeline tiene solo un elemento y si este se corresponde a un
- * comando interno.
- *
- * REQUIRES: p != NULL
- *
- * ENSURES:
- *
- * builtin_alone(p) == pipeline_length(p) == 1 &&
- *                     builtin_is_internal(pipeline_front(p))
- *
- *
- */
+void builtin_run(scommand cmd){
 
-void builtin_run(scommand cmd);
 //Los dos B)
 /*
  * Ejecuta un comando interno
@@ -36,3 +26,4 @@ void builtin_run(scommand cmd);
  * REQUIRES: {builtin_is_internal(cmd)}
  *
  */
+}
