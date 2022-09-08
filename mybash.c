7831d3653521a98294b6231a7be2b0b2cac4692b
Milagros Carabelos <mili.carabelos03@gmail.com>
2022-09-08 17:28:52 -0300

    idea inicial de mybash

--- mybash.c
+++ mybash.c
@@ -8,29 +8,43 @@
 #include "parsing.h"
 #include "builtin.h"
 
-static void show_prompt(void) {
-    printf ("mybash> ");
-    fflush (stdout);
+static void show_prompt(void)
+{
+    printf("mybash> ");
+    fflush(stdout);
 }
+/* 
+int check_for_EOF()
+{
+    if (feof(stdin))
+        return 1;
+    int c = getc(stdin);
+    if (c == EOF)
+        return 1;
+    ungetc(c, stdin);
+} */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     pipeline pipe;
     Parser input;
     bool quit = false;
 
     input = parser_new(stdin);
-    while (!quit) {
+    while (!quit)
+    {
         show_prompt();
         pipe = parse_pipeline(input);
-
+        execute_pipeline(pipe);
         /* Hay que salir luego de ejecutar? */
+        // spoiler no, hay que salir cuando hagamos el ctrl+d
         quit = parser_at_eof(input);
         /*
          * COMPLETAR
          *
          */
     }
-    parser_destroy(input); input = NULL;
+    parser_destroy(input);
+    input = NULL;
     return EXIT_SUCCESS;
 }
-
