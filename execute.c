336361694f5294405687ef96e95a430a1011c912
Tomas Bazan <tomas.pablo.bazan@mi.unc.edu.ar>
2022-09-09 15:43:45 -0300

    Fix execute para usar builtin y incluir libreria para testing

--- execute.c
+++ execute.c
@@ -5,7 +5,9 @@
 #include <sys/types.h> //wait
 #include <sys/wait.h>  //wait
 #include <fcntl.h>
+#include <assert.h>
 
+#include "tests/syscall_mock.h"
 #include "strextra.h"
 #include "command.h"
 #include "execute.h"
@@ -13,146 +15,139 @@
 #include "parsing.h"
 #include "builtin.h"
 
-/* FUNCION KK
-static char ** args (pipeline apipe){
-
-    char ** args = NULL;
-    scommand simple_comand = pipeline_front(apipe);
-    scommand aux = simple_comand;
-    unsigned int elemnum;
-
-    while (aux != NULL)
-    {
-        char * str = scommand_front(aux);
-        args = realloc (elemnum,sizeof(str));
-    }
-
-    return args
-} */
-
 void execute_pipeline(pipeline apipe)
 {
+    assert(apipe != NULL);
+
     scommand simple_command;
     unsigned int length, length2;
     int file;
     char *path;
     pid_t pid;
     int tube[2];
-
-    if (pipeline_length(apipe) == 1)
+    if (builtin_is_internal(pipeline_front(apipe)))
     {
-        pid = fork();
-
-        if (pid < 0) // error
+        simple_command = pipeline_front(apipe);
+        builtin_run(simple_command);
+    }
+    else
+    {
+        if (pipeline_length(apipe) == 1)
         {
-            printf("Fork failure, where PID: %d \n", pid);
-            exit(1);
-        }
-        if (pid == 0) // hijo
-        {
-            simple_command = pipeline_front(apipe);
-            char *in = scommand_get_redir_in(simple_command);
-            char *out = scommand_get_redir_out(simple_command);
-            length = scommand_length(simple_command);
-            char **args = calloc(length, sizeof(char *));
-            scommand_to_array(simple_command, args);
+            pid = fork();
 
-            if (in != NULL)
+            if (pid < 0) // error
             {
-                path = strmerge("./", in);
-                file = open(path, O_RDONLY);
-                if (file < 0)
+                printf("Fork failure, where PID: %d \n", pid);
+                exit(1);
+            }
+            if (pid == 0) // hijo
+            {
+                simple_command = pipeline_front(apipe);
+                char *in = scommand_get_redir_in(simple_command);
+                char *out = scommand_get_redir_out(simple_command);
+                length = scommand_length(simple_command);
+                char **args = calloc(length, sizeof(char *));
+                scommand_to_array(simple_command, args);
+
+                if (in != NULL)
                 {
-                    printf("Error, file doesn't exist.");
+                    path = strmerge("./", in);
+                    file = open(path,1, O_RDONLY);
+                    if (file < 0)
+                    {
+                        printf("Error, file doesn't exist.");
+                    }
+                    else
+                    {
+                        dup2(file, 0);
+                        file = close(file);
+                        if (file < 0)
+                        {
+                            printf("Error while closing the file.");
+                        }
+                    }
                 }
-                else
+                else if (out != NULL)
                 {
-                    dup2(file, 0);
+                    path = strmerge("./", out);
+                    file = open(path,1, O_CREAT);
+                    dup2(file, 1);
                     file = close(file);
                     if (file < 0)
                     {
                         printf("Error while closing the file.");
                     }
                 }
+                execvp(args[0], args);
             }
-            else if (out != NULL)
+            else // padre
             {
-                path = strmerge("./", out);
-                file = open(path, O_CREAT);
-                dup2(file, 1);
-                file = close(file);
-                if (file < 0)
+                if (pipeline_get_wait(apipe))
                 {
-                    printf("Error while closing the file.");
+                    wait(NULL);
                 }
             }
-            execvp(args[0], args);
         }
-        else // padre
-        {
-            if (pipeline_get_wait(apipe))
-            {
-                wait(NULL);
-            }
-        }
-    }
-    else
-    { // dos pipeline
+        else
 
-        // usar funcion pipe
-        pid = fork();
-        pipe(tube);
+        { // dos pipeline
 
-        if (pid < 0) // error
-        {
-            printf("fork first child faliure %d \n", pid);
-            exit(1);
-        }
-        else if (pid == 0) // primer hijo hijo
-        {
-            // creando los argumentos para execvp()
-            scommand simple_command = pipeline_front(apipe);
-            length = scommand_length(simple_command);
-            char **args = calloc(length, sizeof(char *));
-            scommand_to_array(simple_command, args);
-
-            // ver
-            dup2(tube[1], 1);
-            close(tube[1]);
-            execvp(args[0], args);
-        }
-
-        else // padre primerizo
-        {
+            // usar funcion pipe
             pid = fork();
+            pipe(tube);
 
             if (pid < 0) // error
             {
-                printf("fork second child faliure %d \n", pid);
+                printf("fork first child faliure %d \n", pid);
                 exit(1);
             }
-            else if (pid == 0) // segundo hijo
+            else if (pid == 0) // primer hijo hijo
             {
-                pipeline_pop_front(apipe);
+                // creando los argumentos para execvp()
                 scommand simple_command = pipeline_front(apipe);
-                length2 = scommand_length(simple_command);
-                char **args2 = calloc(length2, sizeof(char *));
-                scommand_to_array(simple_command, args2);
+                length = scommand_length(simple_command);
+                char **args = calloc(length, sizeof(char *));
+                scommand_to_array(simple_command, args);
 
-                dup2(tube[0], 0);
-                close(tube[0]);
-                execvp(args2[0], args2);
+                // ver
+                dup2(tube[1], 1);
+                close(tube[1]);
+                execvp(args[0], args);
             }
-            else // padre finalmente
+
+            else // padre primerizo
             {
-                if (pipeline_get_wait(apipe)) // si tenemos "&"
+                pid = fork();
+
+                if (pid < 0) // error
                 {
-                    wait(NULL); // un hijo
-                    wait(NULL); // dos hijos
+                    printf("fork second child faliure %d \n", pid);
+                    exit(1);
                 }
+                else if (pid == 0) // segundo hijo
+                {
+                    pipeline_pop_front(apipe);
+                    scommand simple_command = pipeline_front(apipe);
+                    length2 = scommand_length(simple_command);
+                    char **args2 = calloc(length2, sizeof(char *));
+                    scommand_to_array(simple_command, args2);
+
+                    dup2(tube[0], 0);
+                    close(tube[0]);
+                    execvp(args2[0], args2);
+                }
+                else // padre finalmente
+                {
+                    if (pipeline_get_wait(apipe)) // si tenemos "&"
+                    {
+                        wait(NULL); // un hijo
+                        wait(NULL); // dos hijos
+                    }
+                }
+                close(tube[0]);
+                close(tube[1]);
             }
-            close(tube[0]);
-            close(tube[1]);
         }
     }
 }
